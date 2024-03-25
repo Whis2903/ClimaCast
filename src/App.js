207cf4600698c116bc23b0b93edbd24acb7692e7
Vishal Mohan Nair <vishalmadridrox@gmail.com>
2024-03-25 14:40:56 +0530

    color changes

--- src/App.js
+++ src/App.js
@@ -192,7 +192,7 @@ function App() {
 
             <UTCDatetime />
             <Link
-              href="https://github.com/Amin-Awinti"
+              href="https://github.com/Whis2903"
               target="_blank"
               underline="none"
               sx={{ display: 'flex' }}
